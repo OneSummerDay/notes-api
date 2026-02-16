fb564496fc1765d266ddcc5886ad153ea5a1a0bd
OneSummerDay <artem.kraievyi@gmail.com>
2026-02-16 21:43:20 +0200

    feat: switch notes storage from memory to SQLite database

--- app/main.py
+++ app/main.py
@@ -1,9 +1,23 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
+import sqlite3
 
-app = FastAPI(title="Notes API", description="A simple API to manage notes", version="1.0.0")
 
-notes: list[dict] = []
+app = FastAPI(title="Notes API", description="A simple API to manage notes", version="2.0.0")
+
+conn = sqlite3.connect("notes.db", check_same_thread=False)
+
+cursor = conn.cursor()
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS notes (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    title TEXT NOT NULL,
+    content TEXT NOT NULL
+)
+""")
+
+conn.commit()
 
 class CreateNote(BaseModel):
     title: str
@@ -12,40 +26,53 @@ class CreateNote(BaseModel):
 
 @app.post("/notes", status_code=201)
 def create_note(note: CreateNote):
-    note_id = len(notes) + 1
-
-    new_note = {
-        "id": note_id, 
-        "title": note.title, 
+    cursor.execute("INSERT INTO notes (title, content) VALUES (?, ?)", (note.title, note.content))
+    conn.commit()
+    note_id = cursor.lastrowid
+    return {
+        "id": note_id,
+        "title": note.title,
         "content": note.content
-        }
-    
-    notes.append(new_note)
-
-    return new_note
+    }
 
 
 @app.get("/notes")
 def get_all_notes():
+    cursor.execute("SELECT id, title, content FROM notes")
+    rows = cursor.fetchall()
+    notes = []
+    for row in rows:
+        notes.append({
+            "id": row[0],
+            "title": row[1],
+            "content": row[2]
+        })
+
     return notes
 
 
-@app.get("/notes/(note_id)")
+@app.get("/notes/{note_id}")
 def get_note_by_id(note_id: int):
-    for note in notes:
-        if note["id"] == note_id:
-            return note
+    cursor.execute("SELECT id, title, content FROM notes WHERE id = ?", (note_id))
+    row = cursor.fetchone()
+    if row is None:
+        raise HTTPException(status_code=404, detail="Note not found")
     
-    raise HTTPException(status_code=404, detail="Note not found")
+    return {
+        "id": row[0],
+        "title": row[1],
+        "content": row[2]
+    }
 
 
 @app.delete("/notes/{note_id}", status_code=204)
 def delete_note_by_id(note_id: int):
-    for note in notes:
-        if note["id"] == note.id:
-            notes.remove(note)
-            return {"message": "Note deleted successfully"}
-        
-    raise HTTPException(status_code=404, detail="Note not found")
+    cursor.execute("DELETE FROM notes WHERE id = ?", (note_id))
+    conn.commit()
+    if cursor.rowcount == 0:
+        raise HTTPException(status_code=404, detail="Note not found")
+    conn.commit()
+
+    return {"message": "Note deleted successfully"}
 
 
